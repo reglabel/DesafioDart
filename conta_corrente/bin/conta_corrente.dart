dec9b7b8d4c3a4487fecb99c70c43cf3234d7b81
EngMarianaBrito <marianabrito1791@gmail.com>
2022-04-05 02:11:12 -0300

    Analisando possiveis formas de exexução

--- conta_corrente/bin/conta_corrente.dart
+++ conta_corrente/bin/conta_corrente.dart
@@ -1,5 +1,1 @@
-import 'package:conta_corrente/conta_corrente.dart' as conta_corrente;
 
-void main(List<String> arguments) {
-  print('Hello world: ${conta_corrente.calculate()}!');
-}
--- conta_corrente/test/conta_corrente_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:conta_corrente/conta_corrente.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}

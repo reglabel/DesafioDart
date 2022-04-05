f642296c5d4fbb34a22d4d79ae6c356541ecb786
EngMarianaBrito <marianabrito1791@gmail.com>
2022-04-05 00:22:48 -0300

    Iniciando exercicio

---
+++ conta_corrente/bin/conta_corrente.dart
@@ -0,0 +1,5 @@
+import 'package:conta_corrente/conta_corrente.dart' as conta_corrente;
+
+void main(List<String> arguments) {
+  print('Hello world: ${conta_corrente.calculate()}!');
+}
---
+++ conta_corrente/test/conta_corrente_test.dart
@@ -0,0 +1,8 @@
+import 'package:conta_corrente/conta_corrente.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
